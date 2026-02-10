86aa650c5ad6516386483b338f37e09b6fe2cbc6
Waqas Shami <waqasshami@gmail.com>
2026-02-10 19:16:59 +0100

    Update author title to Data Platform Owner

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -242,7 +242,7 @@ def render_sidebar():
         st.markdown("## 👤 Author")
         st.markdown("""
         **Waqas Shami**
-        Head of Data Platform
+        Data Platform Owner
 
         [LinkedIn](https://linkedin.com/in/waqas-shami) | [Website](https://waqasshami.com)
         """)
